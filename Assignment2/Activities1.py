960610abbe8db895e8f853a4b87cdbeef2ac99c3
rtantuwaya <r_tantuwaya@mail.harpercollege.edu>
2025-09-07 01:21:07 -0500

    Add files via upload

---
+++ Assignment2/Activities1.py
@@ -0,0 +1,24 @@
+﻿#Create a program that asks users for their weight in pounds and their height in feet 
+#and inches.Calculate and display their BMI.
+
+print("Welcome to the BMI Calculator!")
+
+print("\n")
+
+weight = float(input("Enter your weight in pounds: "))
+heightFeet = int(input("Enter your height - Feet: "))
+heightInches = int(input("Enter your height - Inches: "))
+
+totalHeightIn = (heightFeet * 12) + heightInches
+heightMeter = totalHeightIn * 0.0254
+weightKg = weight * 0.453592
+
+bmi = weight /(heightMeter ** 2)
+
+print(f"\nYour BMI is: {bmi:.1f}")
+
+print("\nBMI Categories (WHO Reference):") 
+print("Underweight   : BMI < 18.5")
+print("Normal weight : 18.5 ≤ BMI < 25")
+print("Overweight    : 25 ≤ BMI < 30")
+print("Obese         : BMI ≥ 30")
