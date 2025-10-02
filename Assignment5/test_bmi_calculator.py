92fc085de2960b272846c84e4227cc0df3434b48
rtantuwaya <r_tantuwaya@mail.harpercollege.edu>
2025-10-02 10:14:12 -0500

    Add files via upload

---
+++ Assignment5/test_bmi_calculator.py
@@ -0,0 +1,27 @@
+# a test plan for the BMI program. 
+
+from bmi_functions import calculatorBMI, calculatorBMI_inches
+
+def test_calculatorBMI():
+    bmi = calculatorBMI(150, 5, 10)
+    assert round(bmi, 1) == 21.5
+
+def test_calculatorBMI_inches():
+    bmi = calculatorBMI_inches(150, 70)
+    assert round(bmi, 1) == 21.5
+
+def test_calculatorBMI():
+    bmi = calculatorBMI(170, 5, 10)
+    assert round(bmi, 1) == 22.5
+
+def test_calculatorBMI_inches():
+    bmi = calculatorBMI_inches(120, 70)
+    assert round(bmi, 1) == 31.5
+
+def test_calculatorBMI():
+    bmi = calculatorBMI(150, 5, 10)
+    assert round(bmi, 1) == 18.5
+
+def test_calculatorBMI():
+    bmi = calculatorBMI(200, 5, 10)
+    assert round(bmi, 1) == 28.5
